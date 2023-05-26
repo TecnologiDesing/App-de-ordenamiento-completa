e2844179ce1864944cadc5eb2993e8dbd379f33a
santiago velasquez <lsvelasquez06gmail.com>
2023-05-26 06:19:16 -0500

    se agrego clase_numeros

--- app de ordenamiento/app de ordenamiento/Form1.cs
+++ app de ordenamiento/app de ordenamiento/Form1.cs
@@ -16,5 +16,91 @@ namespace app_de_ordenamiento
         {
             InitializeComponent();
         }
+
+        class Numeros
+        {
+
+
+            private int Longitud;
+            private int[] Arreglo = new int[1];
+            private Button[] Arreglos_Botones = new Button[1];
+
+            public Numeros()
+            {
+
+
+                int a = 0;
+                Arreglo[0] = a;
+                Arreglos_Botones[0] = new Button();
+                Arreglos_Botones[0].Width = 40;
+                Arreglos_Botones[0].Height = 40;
+                Arreglos_Botones[0].BackColor = Color.GreenYellow;
+                Arreglos_Botones[0].Text = a.ToString();
+                Calcular_Longitud();
+
+
+            }
+
+            public void Calcular_Longitud()
+            {
+
+
+                Longitud = Arreglos_Botones.Length;
+
+
+            }
+
+            public int Obtener_Longitud()
+            {
+
+
+                return Longitud;
+
+
+            }
+
+            public int[] Obtener_Arreglo()
+            {
+
+
+                return Arreglo;
+
+
+            }
+
+
+
+            public void Insertar_Dato(int Dato)
+            {
+
+
+                Array.Resize<int>(ref Arreglo, Longitud + 1);
+                Arreglo[Longitud] = Dato;
+
+                Array.Resize<Button>(ref Arreglos_Botones, Longitud + 1);
+
+
+                Arreglos_Botones[Longitud] = new Button();
+                Arreglos_Botones[Longitud].Width = 50;
+                Arreglos_Botones[Longitud].Height = 50;
+                Arreglos_Botones[Longitud].BackColor = Color.GreenYellow;
+                Arreglos_Botones[Longitud].Text = Dato.ToString();
+                Calcular_Longitud();
+
+
+            }
+
+            public Button[] Arreglo_Botones()
+            {
+
+
+                return Arreglos_Botones;
+
+
+            }
+
+
+        }
+
     }
 }
