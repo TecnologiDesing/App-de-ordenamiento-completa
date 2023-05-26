38ce1f93b3259a0fc5e1d84a6e29abb80647ac04
santiago velasquez <lsvelasquez06gmail.com>
2023-05-26 06:40:00 -0500

    se agrego BubbleSort

--- app de ordenamiento/app de ordenamiento/Form1.cs
+++ app de ordenamiento/app de ordenamiento/Form1.cs
@@ -16,5 +16,37 @@ namespace app_de_ordenamiento
         {
             InitializeComponent();
         }
+
+        public void BubbleSort(ref int[] Arreglos, ref Button[] Arreglo_De_Numeros)
+        {
+
+
+            for (int i = 0; i < Arreglos.Length; i++)
+            {
+
+                for (int j = 0; j < Arreglos.Length - 1; j++)
+                {
+
+
+                    if (Arreglos[j] > Arreglos[j + 1])
+                    {
+
+
+                        int aux = Arreglos[j];
+                        Arreglos[j] = Arreglos[j + 1];
+                        Arreglos[j + 1] = aux;
+
+                        Intercambio(ref Arreglo_De_Numeros, j + 1, j);
+
+                    }
+
+
+                }
+
+            }
+
+
+        }
+
     }
 }
