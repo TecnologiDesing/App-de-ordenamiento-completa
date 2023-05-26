4451eda14ad161483c7c5862e1a905e88514f493
santiago velasquez <lsvelasquez06gmail.com>
2023-05-26 05:37:35 -0500

    se agrego BtnAgregar

--- app de ordenamiento/app de ordenamiento/Form1.cs
+++ app de ordenamiento/app de ordenamiento/Form1.cs
@@ -16,5 +16,34 @@ namespace app_de_ordenamiento
         {
             InitializeComponent();
         }
+
+        private void btnAgregar_Click(object sender, EventArgs e)
+        {
+
+            try
+            {
+
+
+                int numero = Convert.ToInt32(txtNumero.Text);
+                Datos.Insertar_Dato(numero);
+                Arreglos_De_Numeros = Datos.Obtener_Arreglo();
+
+                Arreglos = Datos.Arreglo_Botones();
+
+
+            }
+            catch
+            {
+
+
+                MessageBox.Show("Solo se admiten numeros enteros");
+
+
+            }
+
+            estado = true;
+            tabPage1.Refresh();
+
+        }
     }
 }
