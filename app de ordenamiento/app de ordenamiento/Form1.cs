32c42a47d7731aaf0621d46baaadcf9c9fdad4ef
santiago velasquez <lsvelasquez06gmail.com>
2023-05-26 06:51:22 -0500

    se agrego Intercambio

--- app de ordenamiento/app de ordenamiento/Form1.cs
+++ app de ordenamiento/app de ordenamiento/Form1.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -16,5 +17,65 @@ namespace app_de_ordenamiento
         {
             InitializeComponent();
         }
+
+        public void Intercambio(ref Button[] boton, int a, int b)
+        {
+
+            string temp = boton[a].Text;
+
+            Point pa = boton[a].Location;
+
+            Point pb = boton[b].Location;
+
+            int diferencia = pa.X - pb.X;
+            int x = 10;
+            int y = 10;
+            int t = 70;
+
+            while (y != 70)
+            {
+
+
+                Thread.Sleep(t);
+                boton[a].Location += new Size(0, 10);
+                boton[b].Location += new Size(0, -10);
+                y += 10;
+
+
+            }
+
+            while (x != diferencia + 10)
+            {
+
+                Thread.Sleep(t);
+                boton[a].Location += new Size(-10, 0);
+                boton[b].Location += new Size(10, 0);
+                x += 10;
+
+            }
+
+            y = 0;
+
+            while (y != -60)
+            {
+
+                Thread.Sleep(t);
+                boton[a].Location += new Size(0, -10);
+                boton[b].Location += new Size(0, +10);
+                y -= 10;
+            }
+
+
+
+
+            boton[a].Text = boton[b].Text;
+            boton[b].Text = temp;
+            boton[b].Location = pb;
+            boton[a].Location = pa;
+            estado = true;
+            tabPage1.Refresh();
+
+        }
+
     }
 }
